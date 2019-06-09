290a205bbfd5f5dc1ad8b26c02eff8744b9e6f46
bugong <1761869346@qq.com>
2019-06-09 19:53:42 +0800

    edit: page home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,54 +1,118 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Pagination from '../../components/Pagination/Pagination';
 import Tabs from "./Tabs/Tabs";
 
 class PageHome extends React.PureComponent {
 
-   render() {
+    state = {
+        items: [{
+            id: '1',
+            url: '',
+            category: '',
+            categoryName: '精品',
+            title: '这是标题',
+            createBy: {
+                url: '',
+                avatarUrl: 'static/images/avatars/avatar.jpg',
+                username: 'admin'
+            },
+            createTime: '2019-11-11 11:11',
+            commentCount: 109,
+            viewCount: 200,
+            lastCommentBy: {
+                url: '',
+                avatarUrl: 'static/images/avatars/avatar.jpg',
+                username: 'admin'
+            },
+            lastCommentTime: '2019-12-12 12:12'
+        }, {
+            id: '2',
+            url: '',
+            category: '',
+            categoryName: '精品',
+            title: '这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题',
+            createBy: {
+                url: '',
+                avatarUrl: 'static/images/avatars/avatar.jpg',
+                username: 'admin'
+            },
+            createTime: '2019-11-11 11:11',
+            commentCount: 109,
+            viewCount: 200,
+            lastCommentBy: {
+                url: '',
+                avatarUrl: 'static/images/avatars/avatar.jpg',
+                username: 'admin'
+            },
+            lastCommentTime: '2019-12-12 12:12'
+        }]
+    };
 
-       return (
-           <>
-               <div className="panel-header">
+
+    render() {
+
+        return (
+            <>
+                <div className="panel-header">
                     <Tabs/>
-               </div>
-               <div className="panel-content">
-                   <div v-for="(topic, index) in topics" className="topic">
-                       <div className="avatar">
-                           <a href={"/"}><img src="static/images/avatars/avatar.jpg"/></a>
-                       </div>
-                       <div className="info">
-                           <dl>
-                               <dt>
-                                   <span className="tag tag-category">topic.categoryNameCn</span>
-                                   <a href={"/"}>topic.title</a>
-                               </dt>
-                               <dd>
-                                        <span className="item"><a href={"/"}>topic.username</a>
-                                        发布于 <span className="datetime-ago">topic.rtime</span></span>
-                                   <span className="item">• <i
-                                       className="reply-count">topic.commentCount</i> 个回复</span>
-                                   <span className="item">• <i
-                                       className="view-count">topic.viewCount</i> 次浏览</span>
+                </div>
+                <div className="panel-content">
 
-                                   <span className="item right">最后由 <a href={"/"}>
-                                          <img src="static/images/avatars/avatar.jpg"/> username</a> 回复于
-                                  <span className="datetime-ago">5 个月前</span>
-                  </span>
-                               </dd>
-                           </dl>
-                       </div>
-                   </div>
+                    {
+                        this.state.items.length && this.state.items.map((item) => {
 
-                   <div className="no-data">
-                       暂无数据
-                   </div>
-               </div>
-               <div className="panel-footer">
-                   <Pagination/>
-               </div>
-           </>
-       );
-   }
+                            return (
+                                <div className="topic" key={item.id}>
+                                    <div className="avatar">
+                                        <Link to={item.createBy.url}><img src={item.createBy.avatarUrl}/></Link>
+                                    </div>
+                                    <div className="info">
+                                        <dl>
+                                            <dt>
+                                                <span className="tag tag-category">{item.categoryName}</span>
+                                                <Link to={item.url}> {item.title}</Link>
+                                            </dt>
+                                            <dd>
+                                                <span className="item">
+                                                    <Link to={item.createBy.url}>{item.createBy.username}</Link>
+                                                    发布于 <span className="datetime-ago">{item.createTime}</span>
+                                                </span>
+                                                <span className="item">
+                                                    • <i className="reply-count">{item.commentCount}</i> 个回复
+                                                </span>
+                                                <span className="item">
+                                                    • <i className="view-count">{item.viewCount}</i> 次浏览
+                                                </span>
+                                                <span className="item right">
+                                                    最后由
+                                                    <Link to={item.lastCommentBy.url}>
+                                                        <img
+                                                            src={item.lastCommentBy.avatarUrl}/> {item.lastCommentBy.username}
+                                                    </Link>
+                                                    回复于
+                                                    <span className="datetime-ago">{item.lastCommentTime}</span>
+                                                </span>
+                                            </dd>
+                                        </dl>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
+
+                    {
+                        !this.state.items.length && <div className="no-data">
+                            暂无数据
+                        </div>
+                    }
+                </div>
+                <div className="panel-footer">
+                    <Pagination/>
+                </div>
+            </>
+        );
+    }
 }
 
 export default PageHome;
